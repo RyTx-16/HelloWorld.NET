68126df85e6ff11440a297196ab6c4570e405257
Ryan Taylor <rytx-16@hotmail.com>
2022-09-24 12:48:20 +0100

    Simple helloworld for .net

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;  
+            Console.WriteLine($"\nHello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
