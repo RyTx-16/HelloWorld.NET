71225dbc1074c2441b4427630396bb9073a08fed
Ryan Taylor <rytx-16@hotmail.com>
2022-09-28 17:17:25 +0100

    Formatting DateTime

---
+++ NumbersDates/DateTime/Program.cs
@@ -0,0 +1,38 @@
+﻿// DateTime.Now = current date/time
+DateTime now = DateTime.Now;
+System.Console.WriteLine(now);
+
+// DateTime.Today = current date with time set to 00:00:00 
+DateTime today = DateTime.Today;
+System.Console.WriteLine(today);
+
+// DateOnly/TimeOnly = just date/time
+DateOnly date = DateOnly.FromDateTime(DateTime.Now);
+TimeOnly time = TimeOnly.FromDateTime(DateTime.Now);
+System.Console.WriteLine(date);
+System.Console.WriteLine(time);
+
+// Dates have properties
+System.Console.WriteLine(today.DayOfWeek);
+System.Console.WriteLine(today.DayOfYear);
+
+// Methods to change values
+System.Console.WriteLine();
+now = now.AddDays(2);
+System.Console.WriteLine(now);
+now = now.AddMonths(1);
+System.Console.WriteLine(now);
+now = now.AddYears(5);
+System.Console.WriteLine(now);
+now = now.AddHours(12);
+System.Console.WriteLine(now);
+
+// TimeSpan represents a duration of time
+DateTime AprilFools = new DateTime(now.Year, 4, 1);
+DateTime NewYears = new DateTime(now.Year, 1, 1);
+TimeSpan interval = AprilFools - NewYears;
+System.Console.WriteLine(interval);
+
+// Dates can be compared usinig regular expressions
+System.Console.WriteLine($"{NewYears < AprilFools}");
+System.Console.WriteLine($"{NewYears > AprilFools}");
---
+++ NumbersDates/FormatDates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+DateTime AprFools = new DateTime(2025, 4, 1, 23, 30, 30);
+
+// d : short date
+System.Console.WriteLine($"{AprFools:d}");
+
+// D : full date
+System.Console.WriteLine($"{AprFools:D}");
+
+// f : full date, short time
+System.Console.WriteLine($"{AprFools:f}");
+
+// F : full date, long time
+System.Console.WriteLine($"{AprFools:F}");
+
+// g : general date and time
+System.Console.WriteLine($"{AprFools:g}");
+
+// G: general date and time
+System.Console.WriteLine($"{AprFools:G}");
+
+// Format using spec. CultureInfo -> 'System.Globalization'
+System.Console.WriteLine(AprFools.ToString("d", CultureInfo.CreateSpecificCulture("de-DE")));
+
+// Custom formats
+System.Console.WriteLine($"{AprFools:dddd, MMMM, d yyyy}");
+System.Console.WriteLine($"{AprFools:ddd, h:mm:ss tt}");
+System.Console.WriteLine($"{AprFools:dd, h:mm:ss tt}");
+System.Console.WriteLine($"{AprFools:MMM d yyyy}");
