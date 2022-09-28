38f0ec07ce5902f0e4966898bb6c0ad56821d55f
Ryan Taylor <rytx-16@hotmail.com>
2022-09-28 14:43:15 +0100

    String manipulating

---
+++ Strings/Formatting/Program.cs
@@ -0,0 +1,11 @@
+﻿float f1 = 12.3f;
+int i1 = 200;
+
+// Spacing & Alignment 
+
+Console.WriteLine("{0, -15} {1, 10}", "Float Val", "Int Val");
+Console.WriteLine("{0, -15} {1, 10}", f1, i1);
+
+// Spacing & Alignment Interpolation
+Console.WriteLine("{0, -15} {1, 10}", "Float Val", "Int Val");
+Console.WriteLine($"{f1, -15} {i1, 10}");
---
+++ Strings/Manipulation/Program.cs
@@ -0,0 +1,41 @@
+﻿string str1 = "The quick brown fox jumps over the lazy dog.";
+string str2 = "This is a string";
+string str3 = "THIS is a STRING";
+string[] strs = {"one", "two", "three", "four"};
+
+// String length
+// Console.WriteLine(str1.Length);
+
+// Individual Chars
+// Console.WriteLine(str1[14]);
+
+// Iterate over string
+// foreach (char ch in str1) {
+//     Console.Write(ch);
+//     if (ch == 'x') {
+//         System.Console.WriteLine("");
+//         break;
+//     }
+// }
+
+// String concatenation
+string outstr;
+outstr = String.Concat(strs);
+System.Console.WriteLine(outstr);
+
+// Join strings
+outstr = String.Join('.', strs);
+System.Console.WriteLine(outstr);
+outstr = String.Join("---", strs);
+System.Console.WriteLine(outstr);
+
+// String comparison
+bool isEqual = str2.Equals(str3);
+System.Console.WriteLine($"\n{isEqual}");
+
+int res = String.Compare(str2, "This is a string");
+System.Console.WriteLine($"{res}");
+
+// Replacing
+string outstr1 = str1.Replace("fox", "cat");
+System.Console.WriteLine($"\n{outstr1}");
