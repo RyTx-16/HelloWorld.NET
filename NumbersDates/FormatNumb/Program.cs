fa85e0c52b383154f4cd88709dc632e9e657a709
Ryan Taylor <rytx-16@hotmail.com>
2022-09-28 16:06:58 +0100

    Number formatting

---
+++ NumbersDates/FormatNumb/Program.cs
@@ -0,0 +1,33 @@
+﻿// General format is: {index[alignment],spacing:[format]}
+// Types are: N (Number), G (General ), F (Fixed-point),
+// E (Exponential), D (Decimal), P (Percent), X (Hexadecimal),
+// C (Currency)
+
+
+int[] quaters = {1, 2, 3, 4};
+int[] sales = {10000, 150000, 200000, 225000};
+double[] intlMixPct = {.386, .413, .421, .457};
+int val1 = 1234;
+decimal val2 = 1234.5678m;
+
+
+// Index + Format
+System.Console.WriteLine($"{val1:D}, {val1:N}, {val1:F}, {val1:G},");
+System.Console.WriteLine($"{val2:E}, {val2:N}, {val2:F}, {val2:G},");
+
+// Add number after format to spec. precision
+System.Console.WriteLine($"{val1:D6}, {val1:N2}, {val1:F1}, {val1:G3},");
+
+// Format with alignment + Spacing
+System.Console.WriteLine("\nSales by Quater: ");
+System.Console.WriteLine($"{quaters[0]} {quaters[1]} {quaters[2]} {quaters[3]}");
+System.Console.WriteLine($"{sales[0]} {sales[1]} {sales[2]} {sales[3]}");
+System.Console.WriteLine("International Sales:");
+System.Console.WriteLine($"{intlMixPct[0]} {intlMixPct[1]} {intlMixPct[2]} {intlMixPct[3]}");
+
+
+System.Console.WriteLine("\nSales by Quater: ");
+System.Console.WriteLine($"{quaters[0],12} {quaters[1],12} {quaters[2],12} {quaters[3],12}");
+System.Console.WriteLine($"{sales[0],12:C0} {sales[1],12:C0} {sales[2],12:C0} {sales[3],12:C0}");
+System.Console.WriteLine("International Sales:");
+System.Console.WriteLine($"{intlMixPct[0],12:P0} {intlMixPct[1],12:P0} {intlMixPct[2],12:P0} {intlMixPct[3],12:P0}");
---
+++ NumbersDates/Parsing/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] numbStrs = {"  1  ", "   1.45", "-100", "5e+04  "};
+
+int testint;
+float testfloat;
+bool result;
+
+// Parse method to turn string to number 
+foreach (string s in numbStrs) {
+    try {
+        testfloat = float.Parse(s);
+        System.Console.WriteLine($"Parsed no (f) is : {testfloat}");
+        testint = int.Parse(s);
+        System.Console.WriteLine($"Parsed no (i) is : {testint}");
+    }
+    catch (FormatException e) {
+        System.Console.WriteLine($"Could not parse '{s}' : {e.Message}");
+    }
+}
+
+// TryParse method returns true is success 
+result = int.TryParse(numbStrs[0], out testint);
+System.Console.WriteLine($"\n{result} -- '{numbStrs[0]}' : {testint}");
+
+result = float.TryParse(numbStrs[1], out testfloat);
+System.Console.WriteLine($"{result} -- '{numbStrs[1]}' : {testfloat}");
+
+result = int.TryParse(numbStrs[2], out testint);
+System.Console.WriteLine($"{result} -- '{numbStrs[2]}' : {testint}");
+
+result = float.TryParse(numbStrs[3], out testfloat);
+System.Console.WriteLine($"{result} -- '{numbStrs[3]}' : {testfloat}");
+
+
+// WILL FAIL
+result = int.TryParse(numbStrs[1], out testint);
+System.Console.WriteLine($"\n{result} -- '{numbStrs[1]}' : {testint}");
