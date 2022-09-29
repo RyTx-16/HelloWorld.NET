24f6585099bb23564a65cb2bcb0323fae62874e7
Ryan Taylor <rytx-16@hotmail.com>
2022-09-29 09:37:56 +0100

    Cleaned up folder

--- FileIO/ReadWrite/Program.cs
+++ FileIO/ReadWrite/Program.cs
@@ -1,2 +1,27 @@
 ﻿// Read/Write to a file
 
+const string fileName = "TestFile.txt";
+
+// WriteAllText - overwrites a file with the given content.
+if (!File.Exists(fileName)) {
+    File.WriteAllText(fileName, "Text in file.");
+// } else {
+//     using(StreamWriter sw = File.CreateText(fileName)) {
+//     sw.WriteLine("New text file.");
+//     }
+}
+
+// ReadAllText - reads all the content from a file.
+string content;
+content = File.ReadAllText(fileName);
+System.Console.WriteLine(content);
+
+// AppendAllText - Adds text to an existing file
+File.AppendAllText(fileName, "Text added to the file.");
+
+// FileStream can be opened and written to until closed.
+using (StreamWriter sw = File.AppendText(fileName)) {
+    sw.WriteLine("Line 1");
+    sw.WriteLine("Line 2");
+    sw.WriteLine("Line 3");
+}
