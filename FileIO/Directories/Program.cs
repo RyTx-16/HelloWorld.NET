662ddbcbf649d1ca86637ffbffc45553718aee80
Ryan Taylor <rytx-16@hotmail.com>
2022-09-29 10:57:56 +0100

    working with file/dir info

---
+++ FileIO/Directories/Program.cs
@@ -0,0 +1,41 @@
+﻿const string dirName = "TestDir";
+
+// Creating directories.
+
+if (!Directory.Exists(dirName)) {
+    Directory.CreateDirectory(dirName);
+} else {
+    Directory.Delete(dirName);
+}
+
+// Get directory path
+string currentPath = Directory.GetCurrentDirectory();
+// System.Console.WriteLine($"Current Directory: {currentPath}");
+
+// Get directory info
+// DirectoryInfo di = new DirectoryInfo(currentPath);
+// System.Console.WriteLine($"{di.Name}");
+// System.Console.WriteLine($"{di.Parent}");
+// System.Console.WriteLine($"{di.CreationTime}");
+
+// Enumerate contents of directories
+System.Console.WriteLine("\n.............\n");
+System.Console.WriteLine("Just directory content: ");
+List<string> theDirs = new List<string>(Directory.EnumerateDirectories(currentPath));
+foreach (string str in theDirs) {
+    System.Console.WriteLine(str);
+}
+System.Console.WriteLine("\n.............\n");
+System.Console.WriteLine("Just files content: ");
+
+theDirs = new List<string>(Directory.EnumerateFiles(currentPath));
+foreach (string str in theDirs) {
+    System.Console.WriteLine(str);
+}
+
+System.Console.WriteLine("\n.............\n");
+System.Console.WriteLine("All directory content: ");
+theDirs = new List<string>(Directory.EnumerateFileSystemEntries(currentPath));
+foreach (string str in theDirs) {
+    System.Console.WriteLine(str);
+}
---
+++ FileIO/FileInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿const string fName = "TestFile.txt";
+
+if (!File.Exists(fName)) {
+    using (StreamWriter sw = File.CreateText(fName)) {
+        sw.WriteLine("A text file.");
+    }
+}
+
+// Get File Info
+System.Console.WriteLine(File.GetCreationTime(fName));
+System.Console.WriteLine(File.GetLastWriteTime(fName));
+System.Console.WriteLine(File.GetLastAccessTime(fName));
+
+File.SetAttributes(fName, FileAttributes.ReadOnly);
+System.Console.WriteLine(File.GetAttributes(fName));
+
+// General file info.
+
+try {
+    FileInfo fi = new FileInfo(fName);
+    System.Console.WriteLine($"\n{fi.Length}");
+    System.Console.WriteLine($"{fi.Directory}");
+    System.Console.WriteLine($"{fi.IsReadOnly}\n");
+} catch (Exception e) {
+    System.Console.WriteLine($"Exception: {e}");
+}
+
+// Manipulate info
+DateTime dt = new DateTime(2022, 7, 1);
+File.SetCreationTime(fName, dt);
+System.Console.WriteLine(File.GetCreationTime(fName));
