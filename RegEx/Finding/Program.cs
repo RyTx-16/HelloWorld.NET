3c6b96e033bed5721cbfca8ffe93058b6053189a
Ryan Taylor <rytx-16@hotmail.com>
2022-09-29 11:41:06 +0100

    simple regexes

---
+++ RegEx/Finding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+string str1 = "The quick brown Fox jumps over the lazy Dog";
+string str2 = "the quick brown fox jumps over the lazy dog";
+
+// IsMath -- determines if conent of str results in a match with the regex.
+Regex CapWords = new Regex (@"[A-Z]\w+");
+System.Console.WriteLine(CapWords.IsMatch(str1));
+System.Console.WriteLine(CapWords.IsMatch(str2));
+
+System.Console.WriteLine("\n............\n");
+
+Regex NoCase = new Regex (@"fox", RegexOptions.IgnoreCase);
+System.Console.WriteLine(NoCase.IsMatch(str1));
+System.Console.WriteLine(NoCase.IsMatch(str2));
+
+System.Console.WriteLine("\n............\n");
+
+// Match returns a single match at a time
+Match m = CapWords.Match(str1);
+while (m.Success) {
+    System.Console.WriteLine($"'{m.Value}' found at position: {m.Index}");
+    m = m.NextMatch();
+}
+
+System.Console.WriteLine("\n............\n");
+
+// Matches returns a collection of Match objects
+
+MatchCollection mc = CapWords.Matches(str1);
+System.Console.WriteLine($"Found {mc.Count} matches:");
+foreach (Match match in mc) {
+    System.Console.WriteLine($"'{match.Value}' found at position: {match.Index}");
+}
---
+++ RegEx/Replacing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+
+string str1 = "The quick brown Fox jumps over the lazy Dog";
+
+Regex CapWords = new Regex (@"[A-Z]\w+");
+
+// Replace content in strings
+string result = CapWords.Replace(str1, "***");
+System.Console.WriteLine(str1);
+System.Console.WriteLine(result);
+
+System.Console.WriteLine("\n............\n");
+
+// Replace text using MatchEvaluator 
+string makeUpper(Match m) {
+    string s = m.ToString();
+    if (m.Index == 0) {
+        return s;
+    }
+    return s.ToUpper();
+}
+
+string uprStr = CapWords.Replace(str1, new MatchEvaluator(makeUpper));
+System.Console.WriteLine(str1);
+System.Console.WriteLine(uprStr);
